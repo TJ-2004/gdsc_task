7806dc18d767430213ede10930500fe5bf5a6639
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-24 16:14:26 +0530

    Improve UI and Functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Preview from "./components/Preview";
 
 const App = () => {
+  const initialMarkdown = `*Tanishq Jaiswal*`;
+  const [text, setText] = useState(initialMarkdown);
+
+  console.log(text);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
   return (
-  <div className="text-3xl text-green-400">
-        Hello World
-  </div>
+    <div className=" flex md:flex-row flex-col h-screen w-screen gap-5 justify-center items-center bg-blue-800">
+      <div className="flex  w-full h-2/5 rounded-xl overflow-hidden   m-9 ">
+        <textarea
+          className="w-full h-full  border-none text-xl  resize-none  text-black"
+          value={text}
+          onChange={handleChange}
+          placeholder="Enter Markdown..."
+          aria-label="Markdown Editor"
+        />
+      </div>
+
+      <div className="flex  w-full h-2/5 rounded-xl overflow-hidden  bg-red-500 m-9 ">
+        <Preview value = {text}/>
+      </div>
+    </div>
   );
 };
 
---
+++ src/components/Preview.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Markdown from "react-markdown";
+import Remarkgfm from "remark-gfm";
+
+const Preview = ({ value }) => {
+  console.log({ value });
+  return (
+    <div className="w-1/2">
+      <Markdown children={value} remarkPlugins={[Remarkgfm]} />
+    </div>
+  );
+};
+
+export default Preview;
