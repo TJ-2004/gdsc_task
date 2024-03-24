db9b512f55eefe494c49ba3194ecf1b348db6818
TJ-2004 <124814940+TJ-2004@users.noreply.github.com>
2024-03-24 17:23:08 +0530

    ✅Add Clear and Copy button

--- src/App.js
+++ src/App.js
@@ -2,27 +2,49 @@ import React, { useState } from "react";
 import Preview from "./components/Preview";
 
 const App = () => {
-  const initialMarkdown = `*Tanishq Jaiswal*`;
-  const [text, setText] = useState(initialMarkdown);
+  const initial = `![Image](https://cdn-images-1.medium.com/max/578/1*vZVM7utCuRiZ6-HDsNeYUA@2x.png)`;
+  const [text, setText] = useState(initial);
 
-  console.log(text);
   const handleChange = (e) => {
     setText(e.target.value);
   };
+  const handleClick = (e) => {
+    setText(" ");
+  };
+
   return (
-    <div className=" flex md:flex-row flex-col h-screen w-screen gap-5 justify-center items-center bg-blue-800">
-      <div className="flex  w-full h-2/5 rounded-xl overflow-hidden   m-9 ">
-        <textarea
-          className="w-full h-full  border-none text-xl  resize-none  text-black"
-          value={text}
-          onChange={handleChange}
-          placeholder="Enter Markdown..."
-          aria-label="Markdown Editor"
-        />
+    <div className=" flex flex-col items-center justify-center h-screen w-screen bg-blue-800">
+      <div className="flex md:flex-row flex-col h-[80%] md:h-2/5 w-[90%] gap-5 justify-center items-center ">
+        <div className=" w-full h-full rounded-xl overflow-hidden   md:m-9 ">
+          <textarea
+            className="w-full h-full  border-none text-xl  resize-none p-4 text-black"
+            value={text}
+            onChange={handleChange}
+            placeholder="Enter Markdown..."
+            aria-label="Markdown Editor"
+          />
+        </div>
+
+        <div className="flex  w-full h-full rounded-xl overflow-y-auto p-4 text-xl bg-white m-9 ">
+          <Preview value={text} />
+        </div>
       </div>
 
-      <div className="flex  w-full h-2/5 rounded-xl overflow-hidden  bg-red-500 m-9 ">
-        <Preview value = {text}/>
+      <div className="flex mt-3  justify-between  w-[85%]">
+        <button
+          type="button"
+          onClick={handleClick}
+          className="text-white rounded-lg cursor-pointer bg-green-600 px-4 py-2"
+        >
+          Clear
+        </button>
+        <button
+          type="button"
+        //   onClick={handleCopy}
+          className="text-white rounded-lg cursor-pointer bg-red-600 px-4 py-2"
+        >
+          Copy
+        </button>
       </div>
     </div>
   );
